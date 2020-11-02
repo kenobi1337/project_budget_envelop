0a95fe5bd4a01f79db60fdf9ff2bd47f7cc4cdb9
miwzerking <0865697@students.dadeschools.net>
2020-11-02 15:09:15 -0500

    created and edit CRUD app

--- route/budget.js
+++ route/budget.js
@@ -1,42 +1,47 @@
 const express = require('express');
 const budget = express.Router();
+const bodyParser = require('body-parser');
 
 module.exports = budget;
 
-const budgets = {};
+const budgets = [];
 
-budget.param('name', (req, res, next, nameId) => {
-    const name = nameId.toLowerCase();
-    if (!budgets[name]) {
-        return res.status(404).send();
+
+
+budget.get('/', (req, res, next) => {
+    res.send(budgets);
+})
+budget.get('/:id', (req, res, next) => {
+    let index = Number(req.params.id);
+    if (index <= budgets.length) {
+        index--;
+        const specific = budgets[index];
+        res.send(specific);
+    } else {
+        res.status(404).send();
     }
-    this.name = name;
-    next();
 })
 budget.post('/', (req, res, next) => {
-    const name = req.query.name.toLowerCase();
-    const budget = req.query.budget;
-    if (name && budget) {
-        budgets[name] = {'budget': Number(budget)};
-        res.status(201).send(budgets[name]);
+    const toAdd = req.body;
+    let length = budgets.length;
+    length+=1;
+    toAdd["id"] = length;
+    if (toAdd.name) {
+        budgets.push(toAdd);
+        res.status(201).send(budgets[budgets.length-1]);
     } else {
         res.status(400).send();
     }
 })
-budget.get('/', (req, res, next) => {
-    res.send(budgets);
-})
-budget.get('/:name', (req, res, next) => {
-    res.send(budgets[this.name]);
-})
-budget.put('/:name', (req, res, next) => {
-    const budget = req.query.budget;
-    budgets[this.name].budget = Number(budget);
-    res.send(budgets[this.name]);
-})
-budget.delete('/:name', (req, res, next) => {
-    delete budgets[this.name];
-    res.status(204).send();
+budget.delete('/:id', (req, res, next) => {
+    let index = req.params.id;
+    if (index <= budgets.length) {
+        index--;
+        budgets.splice(index, 1);
+        res.status(204).send();
+    } else {
+        res.status(404).send();
+    }
 })
 
 
